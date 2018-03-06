24e3d4742d8d3d53fe4175e61a6a0970108cfd1f
Efody <avidane@amazon.com>
2018-03-06 18:29:50 +0200

    reorg

--- start_elastic.sh
+++ start_elastic.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#export JAVA_HOME=$(pwd)/jdk/jdk1.8.0_161
-#export PATH=$JAVA_HOME/bin:$PATH
+export JAVA_HOME=$(pwd)/jdk/jdk1.8.0_161
+export PATH=$JAVA_HOME/bin:$PATH
 
-cd /Users/avidane/elasticsearch-5.5.2/bin
+cd es/elasticsearch-6.2.0/bin
 ./elasticsearch&
 echo "done
